8f5bdb00ed6389cb0bce3d0443c07ab90c1f95d5
Lennard Dorst <lennardsolana@gmail.com>
2023-01-08 21:06:14 +0100

    Adding existing code.

--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,18 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" class="bg-[#093145] max-w-5xl mx-auto">
       <Head />
       <body>
+        <nav class="flex flex-col text-center sm:flex-row sm:text-left sm:justify-between p-4 bold uppercase sm:items-baseline w-full font-bold">
+          <div class="mb-2 sm:mb-0">
+            <a href="/" class="text-blue-400 hover:text-blue-500 bold">LaunchList 🚀</a>
+          </div>
+
+          <div>
+            <a href="https://www.twitter.com/lennardeth" class="text-blue-400 hover:text-blue-500 ml-2 bold">by @lennardeth</a>
+          </div>
+          </nav>
         <Main />
         <NextScript />
       </body>
